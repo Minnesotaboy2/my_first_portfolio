958a6b4a8bba622e2f9a6abb098b490eaf662863
Andrew Martinek <andrewdmartinek@gmail.com>
2022-05-11 18:26:22 -0500

    Work-day-scheduler page is up, updated readme, made changes to copy button in password generator

--- README.md
+++ README.md
@@ -19,5 +19,9 @@ I ended up putting the entire password generator into this page on my portfolio 
 ![pageScrnSht](./assets/images/passwordgen1.png)
 
 ## Code Quiz
-Page contains description of the code-quiz assignment and contains a link to the page.
+Page contains description of the code-quiz assignment and contains a link to the page and repository.
 ![pageScrnSht](./assets/images/code-quiz1.png)
+
+## Work-Day-Scheduler
+Page contains a brief description of the assignment and contains a link to the page and repository.
+![PageScrnSht](./assets/images/workdayscheduler1.png)
--- assets/js/password-gen.js
+++ assets/js/password-gen.js
@@ -6,6 +6,7 @@ const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
 const pass = document.getElementById("password-copy")
+const copy = document.getElementById("copy-btn")
 
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
@@ -33,6 +34,7 @@ form.addEventListener('submit', e => {
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+    copy.style.visibility = "visible"
     pass.style.visibility = "hidden"
     let charCodes = LOWERCASE_CHAR_CODES
     if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
